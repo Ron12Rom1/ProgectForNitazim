833907a830e6e6c35cf4bad632c1ff2380efd8ac
Ron <coder.ronronbruk@gmail.com>
2024-06-14 20:12:30 +0300

    FIxed Itays code and add a bit

--- BlackJak.py
+++ BlackJak.py
@@ -1,12 +1,18 @@
 import random
 
-PARAMETERS = ['color', 'num', 'shape', 'shading']
-COLORS = ['green', 'purple', 'red', 'black']
-NUMBERS = [x for x in range(1,12)]
+NUMBERS = [x for x in range(1,14)]
 SHAPES = ['heart', 'diamond', 'clover', "leaf"]
 NO_SET = -2
 SHOW_HINT = -1
 LEN_BOARD = 12
+
+def create_deck():
+    deck = list()
+    for shape in SHAPES:
+        for num in NUMBERS:
+                deck.append(create_card(shape,  num))
+    deck = shuffle_deck(deck)
+    return deck
 
 def print_board(board):
     for card in board:
@@ -14,58 +20,53 @@ def print_board(board):
 
 
 def shuffle_deck(deck):
-    print("shuffleing..")
+    print("Shuffleing..")
     random.shuffle(deck)
-    print("deck: ", deck)
-
-def create_board(deck):
-    board = list()
-    for i in range(12):
-        board.append(deck[i])
-        deck.remove(deck[i])
-    return board
-
-def take_card():
-    deck = list()
-    for color in COLORS:
-        for shape in SHAPES:
-            for num in NUMBERS:
-                    deck.append(create_card(color, shape, num))
-    shuffle_deck(deck)
+    print("Done!, Ready to play!")
     return deck
 
-def create_card(color, shape, shading, number):
+# def create_board(deck):
+#     board = list()
+#     for i in range(12):
+#         board.append(deck[i])
+#         deck.remove(deck[i])
+#     return board
+
+def take_card(deck):
+    #! to make the function work
+
+    return deck
+
+def create_card(shape, number):
     card = {
-        "color": color,
         "shape": shape,
-        "shading": shading,
         "num": number}
     return card
 
-def play_turn (card):
-    play = int(input("what you want to play 1(take one card) 2(hit) 3(double) 4(split) 5(Stand)"))
-    while play <= 5 and play >= 1:
-        if (play == 1):
-            card = take_card()
-        if (play == 2):
-            cards = int(input("How many cards do you want to take?"))
-            for i in range (cards + 1):
-                card = take_card()
-        if (play == 3):
-            for i in range (3):
-                card = take_card()
-        if (play == 4):
-            print("It can't be done right now and fixed in the future")
-        elif (play == 5):
+#! WHAT THE HECK IS THIS FUNCTION!?!?!
+# def play_turn ():
+#     play = int(input("what you want to play 1(take one card) 2(hit) 3(double) 4(split) 5(Stand)"))
+#     while play <= 5 and play >= 1:
+#         if (play == 1):
+#             card = take_card()
+#         if (play == 2):
+#             cards = int(input("How many cards do you want to take?"))
+#             for i in range (cards + 1):
+#                 card = take_card()
+#         if (play == 3):
+#             for i in range (3):
+#                 card = take_card()
+#         if (play == 4):
+#             print("It can't be done right now and fixed in the future")
+#         elif (play == 5):
+#             pass  ####! This should not be here
             
 def create_player():
     player = {
         "name": input("What is your name?"),
         "money": 100,
-        "cards": list() == 0
+        "cards": []
     }
-
-    print(player)
     return player
 
 
@@ -75,6 +76,7 @@ def create_playersInfo():
     for i in range(player_count):
         players.append(create_player())
     return players
+
 
 ##################################################################
 ##################################################################
@@ -93,7 +95,36 @@ def main():
             else: break
         except ValueError:
             print("Please enter a number.")
+    
         
+        #* Create players
+    players = create_playersInfo()
+    
+        #! give players cards
+
+    
+        #* Get shuffled deck and give players cards
+    deck = create_deck()
+    print(deck)
+
+
+    #* Game Starts
+    #----------------------------------------------------------------
+    while True:
+        
+
+
+
+
+
+    #----------------------------------------------------------------
+
+    #print(take_card())
+
+
+
+    print(players)
+    
     
 
 
@@ -108,6 +139,5 @@ def main():
 
 
 
-#main()
+main()
 
-create_player()
