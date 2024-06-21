64df29b24b9a854e2ae441ad58a610ada7ccea5c
Ron <coder.ronronbruk@gmail.com>
2024-06-21 18:07:07 +0300

    fix score logic

--- singleplayer Blackjak.py
+++ singleplayer Blackjak.py
@@ -1,8 +1,6 @@
 import time
 import random
 
-help(format)
-
 def intro():
     print("Welcome to the gratest game of all games!\n(By Ron Bruk & Itay something)")
     print("""
@@ -30,29 +28,30 @@ def intro():
            \______/                                                                                                          \______/ \n\n⁽ᵀʰᶦˢ ᵍᵃᵐᵉ ᶦˢ ˢᶦⁿᵍᵉˡᵖˡᵃʸᵉʳ⁾\n\n\n\n""")
     
     print("Initializing player...")
-    # time.sleep(2)
+    #time.sleep(2)
     global players
     players = create_playersInfo()
+    #time.sleep(1)
     print("Shuffeling cards...")
-    # time.sleep(2.5)
+    #time.sleep(2.5)
     global deck
     deck = create_deck()
     print("Done! Game is ready.\nGood lack ", players[1]["name"],"!", sep="")
-    # time.sleep(1)
+    #time.sleep(1)
     print("\nGame starts in 5...")
-    # time.sleep(1)
+    #time.sleep(1)
     print("4...")
-    # time.sleep(1)
+    #time.sleep(1)
     print("3...")
-    # time.sleep(1)
+    #time.sleep(1)
     print("2...")
-    # time.sleep(1)
+    #time.sleep(1)
     print("1...")
-    # time.sleep(1)
+    #time.sleep(1)
     print("\n".join([" " for _ in range(40)]))
 
 
-NUMBERS = [x for x in range(1,14)]
+NUMBERS = [int(2),int(3),int(4),int(5),int(6),int(7),int(8),int(9),int(10),"J","Q","K","A"]
 SHAPES = ['heart', 'diamond', 'clover', "leaf"]
 NO_SET = -2
 SHOW_HINT = -1
@@ -96,7 +95,79 @@ def give_players_cards(player, deck):
         card = random.choice(deck)
         deck.remove(card)
         player["cards"].append(card)
+
+def give_players_1_card(deck):
+    card = random.choice(deck)
+    deck.remove(card)
+    player["cards"].append(card)
     
+
+def print_card(card):
+    if card["shape"] == "diamond":
+        print("♦ " + str(card['num']))
+    elif card["shape"] == "heart":
+        print("♥ " + str(card['num']))
+    elif card["shape"] == "clover":
+        print("♣ " + str(card['num']))
+    elif card["shape"] == "leaf":
+        print("♠ " + str(card['num']))
+
+def draw_table_END():
+     print("\n\n\nDealer's cards: ")
+     for card in players[0]["cards"]:
+         print_card(card)
+     print("\nYour cards: ")
+     for card in players[1]["cards"]:
+         print_card(card)
+         print()
+     print("Dealer's score: ", get_score_1_card(players[0]["cards"][0]))
+     print("Your score: ", get_score_1_card(players[1]["cards"][0]))
+    
+def draw_table_1_card():
+    print("\n\n\n\nYour cards: ")
+    for i in range(len(player["cards"])-1):
+        print_card(player["cards"][i])
+    print("You're score: ", get_score_1_card(player["cards"]))
+    print("\n\nDealer's cards: ")
+    print_card(dealer["cards"][0])
+    print("Dealer's score: ", get_score_1_card(dealer["cards"]))
+    
+
+
+def get_score_1_card(cards):
+    score = 0
+    for i in range(len(cards)-1):
+        if cards[i]["num"] == "J":
+            score += 11
+        elif cards[i]["num"] == "Q":
+            score += 12
+        elif cards[i]["num"] == "K":
+            score += 13
+        elif cards[i]["num"] == "A":
+            score += 14
+        else:
+            score += cards[i]["num"]
+    return score
+
+def play_turn (deck):
+    play = int(input("what you want to play: 1(take one card) 2(hit) 3(double) 4(split) 5(Stand)"))
+    while play <= 5 and play >= 1:
+        if (play == 1):
+            give_players_1_card(deck)
+            draw_table_1_card()
+            play = -1
+        if (play == 2):
+            cards = int(input("How many cards do you want to take?"))
+            for i in range (cards + 1):
+                ...
+        if (play == 3):
+            for i in range (3):
+                ...
+        if (play == 4):
+            print("It can't be done right now and fixed in the future")
+        elif (play == 5):
+            pass  
+
 
 def main():
         """Evry one place a bet
@@ -115,9 +186,13 @@ def main():
         print("(Game started)\n\n")
         #print(players)
 
+        global player, dealer
         player = players[1]
+        dealer = players[0]
         give_players_cards(player, deck)
+        give_players_cards(dealer, deck)
         print(player)
+        print(dealer)
 
         print("Please make a bet (you have " + str(player["money"]) + ")")
 
@@ -138,8 +213,9 @@ def main():
                 break
 
         
+        draw_table_1_card()
 
-
+        play_turn(deck)
 
 
 
